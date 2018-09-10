98c4c67f63e71221ac58e153173a556e9eb28e32
Carl Wingårdh <c.a.wingardh@gmail.com>
2018-09-10 15:11:39 +0200

    Commented out textgain tests until its implemented again

--- src/test/java/com/thatvineyard/snapswriter/metre/TextgainInterfaceTest.java
+++ src/test/java/com/thatvineyard/snapswriter/metre/TextgainInterfaceTest.java
@@ -1,59 +1,60 @@
-package com.thatvineyard.snapswriter.metre;
+// package com.thatvineyard.snapswriter.metre;
 
-import static org.junit.Assert.assertEquals;
+// import static org.junit.Assert.assertEquals;
 
-import com.thatvineyard.snapswriter.metre.TextgainInterface;
+// import com.thatvineyard.snapswriter.metre.TextgainInterface;
 
-import org.junit.Test;
+// import org.junit.Test;
 
-/**
- * TextgainInterfaceTest
- */
-public class TextgainInterfaceTest {
+// /**
+// * TextgainInterfaceTest
+// */
+// public class TextgainInterfaceTest {
 
-    @Test
-    public void countZeroSyllables() {
+// @Test
+// public void countZeroSyllables() {
 
-        int actual = TextgainInterface.numberOfSyllablesInString("");
-        int expected = 0;
+// int actual = TextgainInterface.numberOfSyllablesInString("");
+// int expected = 0;
 
-        assertEquals(expected, actual);
-    }
+// assertEquals(expected, actual);
+// }
 
-    @Test
-    public void countOneSyllable() {
+// @Test
+// public void countOneSyllable() {
 
-        int actual = TextgainInterface.numberOfSyllablesInString("and");
-        int expected = 1;
+// int actual = TextgainInterface.numberOfSyllablesInString("and");
+// int expected = 1;
 
-        assertEquals(expected, actual);
-    }
+// assertEquals(expected, actual);
+// }
 
-    @Test
-    public void countTwoSyllables() {
+// @Test
+// public void countTwoSyllables() {
 
-        int actual = TextgainInterface.numberOfSyllablesInString("hello");
-        int expected = 2;
+// int actual = TextgainInterface.numberOfSyllablesInString("hello");
+// int expected = 2;
 
-        assertEquals(expected, actual);
-    }
+// assertEquals(expected, actual);
+// }
 
-    @Test
-    public void countSyllablesInTwoWords() {
+// @Test
+// public void countSyllablesInTwoWords() {
 
-        int actual = TextgainInterface.numberOfSyllablesInString("hello friend");
-        int expected = 3;
+// int actual = TextgainInterface.numberOfSyllablesInString("hello friend");
+// int expected = 3;
 
-        assertEquals(expected, actual);
-    }
+// assertEquals(expected, actual);
+// }
 
-    @Test
-    public void countOneSyllablesWithPunctuation() {
+// @Test
+// public void countOneSyllablesWithPunctuation() {
 
-        int actual = TextgainInterface.numberOfSyllablesInString("hello my friend, how are you doing? I am fine");
-        int expected = 12;
+// int actual = TextgainInterface.numberOfSyllablesInString("hello my friend,
+// how are you doing? I am fine");
+// int expected = 12;
 
-        assertEquals(expected, actual);
-    }
+// assertEquals(expected, actual);
+// }
 
-}
+// }
